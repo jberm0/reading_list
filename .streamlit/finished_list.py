9b7110800fb0847688ec9becf07a40331842890b
jberm0 <jmberman12@gmail.com>
2024-12-12 10:07:59 +0000

    feat: wrap in funcs

--- .streamlit/finished_list.py
+++ .streamlit/finished_list.py
@@ -4,37 +4,40 @@ import sys
 
 sys.path.append("././")
 
-from src.classes import FinishedList, init_db
+from src.classes import init_db
 from src.db import sync_table_to_local_file
 
-st.title("Finished Books")
+def finished_books():
 
-init_db()
+    st.title("Finished Books")
 
-#FinishedList()
-st.dataframe(
-    duckdb.execute(
-        """
-        SELECT finished_id as id, title, author, suggested_by, rating, finished
-        FROM data.finished
-        """
-    ).pl()
-)
+    init_db()
 
-st.sidebar.title("Remove Book")
-with st.sidebar.form("Remove Book"):
-    id = st.number_input("ID", value=None)
-    title = st.text_input("Title", value=None)
-    author = st.text_input("Author", value=None)
+    st.dataframe(
+        duckdb.execute(
+            """
+            SELECT finished_id as id, title, author, suggested_by, rating, finished
+            FROM data.finished
+            """
+        ).pl()
+    )
 
-    submitted = st.form_submit_button("Submit")
+    st.sidebar.title("Remove Book")
+    with st.sidebar.form("Remove Book"):
+        id = st.number_input("ID", value=None)
+        title = st.text_input("Title", value=None)
+        author = st.text_input("Author", value=None)
 
-if submitted:
-    query = f"DELETE FROM data.finished WHERE finished_id = '{id}' AND title = '{title}' AND author = '{author}'"
-    duckdb.execute(query)
-    print(f"Deleted from data.finished where finished_id is {id}, title is {title} and author is {author}")
-    sync_table_to_local_file(schema="data", table="finished", extension="csv")
-    print("Synced to local filesystem")
-    st.write(f"Removed {title} by {author} from finished list")
+        submitted = st.form_submit_button("Submit")
 
-st.button("Clear")
+    if submitted:
+        query = f"DELETE FROM data.finished WHERE finished_id = '{id}' AND title = '{title}' AND author = '{author}'"
+        duckdb.execute(query)
+        print(f"Deleted from data.finished where finished_id is {id}, title is {title} and author is {author}")
+        sync_table_to_local_file(schema="data", table="finished", extension="csv")
+        print("Synced to local filesystem")
+        st.write(f"Removed {title} by {author} from finished list")
+
+    st.button("Clear")
+
+finished_books()
