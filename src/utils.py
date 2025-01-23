ff0935505441a81a79a13263dca1b46007fb0531
jberm0 <jmberman12@gmail.com>
2025-01-23 09:12:39 +0000

    fix id creation

--- src/utils.py
+++ src/utils.py
@@ -5,4 +5,4 @@ def create_id(file_path, column):
     current_count = duckdb.sql(f"SELECT MAX({column})+1 FROM read_csv('{file_path}')").pl()[
         0, 0
     ]
-    return current_count + 1
+    return current_count
